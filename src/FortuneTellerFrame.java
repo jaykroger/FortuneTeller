fa6c406bd973abe7a93cbc1fa6e47423efcada63
jaykroger <jay.kroger123456@gmail.com>
2023-09-23 19:15:59 -0400

    Initial commit

---
+++ src/FortuneTellerFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class FortuneTellerFrame extends JFrame
+{
+    public FortuneTellerFrame()
+    {
+        setTitle("Fortune Teller");
+        setSize(1280, 1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+
+
+}
