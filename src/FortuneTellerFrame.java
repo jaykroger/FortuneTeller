042e5dd56cf831a85bff8910ca8d9242db152fd5
jaykroger <jay.kroger123456@gmail.com>
2023-09-24 13:50:10 -0400

    Finished full FortuneTeller GUI and tested

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class FortuneTellerFrame extends JFrame
 {
@@ -42,18 +44,16 @@ public class FortuneTellerFrame extends JFrame
 
 
         add(mainPanel);
-        setTitle("Fortune Teller");
-        setSize(1280, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
     }
 
     private void createIconPanel()
     {
         iconPanel = new JPanel();
-        icon = new ImageIcon("fortunecookie.png");
+        icon = new ImageIcon("src/fortunecookie.jpg");
         label = new JLabel("Fortune Teller", icon, JLabel.CENTER);
         label.setFont(new Font("Montserrat", Font.PLAIN, 48));
+        label.setVerticalTextPosition(JLabel.BOTTOM);
+        label.setHorizontalTextPosition(JLabel.CENTER);
         iconPanel.add(label);
     }
 
@@ -61,6 +61,7 @@ public class FortuneTellerFrame extends JFrame
     {
         displayPanel = new JPanel();
         textArea = new JTextArea(22, 100);
+        textArea.setFont(new Font("Times New Roman", Font.PLAIN, 12));
         scroller = new JScrollPane(textArea);
         displayPanel.add(scroller);
     }
@@ -72,12 +73,69 @@ public class FortuneTellerFrame extends JFrame
         fortuneButton = new JButton("Read my Fortune!");
         quitButton = new JButton("Quit");
 
-        FortuneButton getFortune = new FortuneButton();
+        fortuneButton.addActionListener((ActionEvent ae) ->
+        {
+            String fortune = getFortune();
+            System.out.println(fortune);
+            textArea.append(fortune + "\n");
+        });
 
-        fortuneButton.addActionListener(getFortune);
         quitButton.addActionListener((ActionEvent ae) -> System.exit(0));
+
+        fortuneButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        quitButton.setFont(new Font("Arial", Font.PLAIN, 20));
 
         buttonPanel.add(fortuneButton);
         buttonPanel.add(quitButton);
+    }
+
+    // Methods used for button functions
+    int lastIndex = -1;
+    public String getFortune()
+    {
+        String fortune_1 = "Any day above ground is a good day.";
+        String fortune_2 = "All your hard work will soon pay off.";
+        String fortune_3 = "Determination is what you need now.";
+        String fortune_4 = "Help! I’m being held prisoner in a chinese bakery!";
+        String fortune_5 = "Now is a good time to buy stock.";
+        String fortune_6 = "Practice makes perfect.";
+        String fortune_7 = "The saints are the sinners who keep on trying.";
+        String fortune_8 = "There is a time for caution, but not for fear.";
+        String fortune_9 = "You can’t steal second base and keep your foot on first.";
+        String fortune_10 = "A faithful friend is a strong defense.";
+        String fortune_11 = "Believe it can be done.";
+        String fortune_12 = "Don’t just spend time. Invest it.";
+
+        ArrayList<String> fortunes = new ArrayList<>();
+
+        fortunes.add(fortune_1);
+        fortunes.add(fortune_2);
+        fortunes.add(fortune_3);
+        fortunes.add(fortune_4);
+        fortunes.add(fortune_5);
+        fortunes.add(fortune_6);
+        fortunes.add(fortune_7);
+        fortunes.add(fortune_8);
+        fortunes.add(fortune_9);
+        fortunes.add(fortune_10);
+        fortunes.add(fortune_11);
+        fortunes.add(fortune_12);
+
+        int index = getRandomInt(0, 11);
+
+        while (index == lastIndex)
+        {
+            index = getRandomInt(0, 11);
+        }
+
+        lastIndex = index;
+        String fortune = fortunes.get(index);
+        return fortune;
+    }
+
+    public int getRandomInt(int Min, int Max)
+    {
+        return ThreadLocalRandom
+                .current().nextInt(Min, Max + 1);
     }
 }
--- src/FortuneTellerViewer.java
+++ src/FortuneTellerViewer.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class FortuneTellerViewer {
+public class FortuneTellerViewer
+{
     public static void main(String[] args)
     {
         FortuneTellerFrame fortuneTeller = new FortuneTellerFrame();
+
+        fortuneTeller.setTitle("Fortune Teller");
+        fortuneTeller.setSize(1080, 560);
+        fortuneTeller.setLocation(100, 80);
+        fortuneTeller.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fortuneTeller.setVisible(true);
     }
 }
